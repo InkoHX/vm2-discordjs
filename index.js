0373096b3e431aedea669390c38520609f1d951b
InkoHX <inkohx@gmail.com>
2020-04-30 21:59:29 +0900

    minor fix

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { VM } = require('vm2')
 const { Client, Util, MessageAttachment } = require('discord.js')
 
 const client = new Client()
-const vm = new VM({ timeout: 5000 })
 
 const codeBlockRegex = /^`{3}(?<lang>[a-z]+)\n(?<code>[\s\S]+)\n`{3}$/um
 const languages = ['js', 'javascript']
@@ -24,6 +23,7 @@ client.on('message', message => {
   if (codeBlock && !languages.includes(codeBlock.lang)) return
 
   try {
+    const vm = new VM({ timeout: 5000 })
     const result = vm.run(codeBlock.code)
 
     return message.reply(toContent(result), { code: 'js' }).catch(console.error)
