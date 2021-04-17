5dc62489ea05186ad1c5d221b7fcf13059400d9b
InkoHX <me@inkohx.dev>
2021-04-17 18:13:31 +0900

    削除方法を選択できるようにする

--- src/structures/message.js
+++ src/structures/message.js
@@ -1,47 +1,99 @@
-const { Structures } = require("discord.js");
+const { Structures, MessageEmbed } = require('discord.js')
 
-module.exports = Structures.extend('Message', BaseClass => class extends BaseClass {
-  /**
-   * @param {import('discord.js').StringResolvable|import('discord.js').APIMessage} content 
-   * @param {import('discord.js').MessageOptions|import('discord.js').MessageAdditions} options 
-   */
-  async sendDeleteable(content, options = {}) {
-    const replies = await this.reply(content, options)
-    const wastebasket = '🗑️'
-    const filter = (reaction, user) =>
-      reaction.emoji.name === wastebasket && user.id === this.author.id
+module.exports = Structures.extend(
+  'Message',
+  BaseClass =>
+    class extends BaseClass {
+      /**
+       * @param {import('discord.js').StringResolvable|import('discord.js').APIMessage} content
+       * @param {import('discord.js').MessageOptions|import('discord.js').MessageAdditions} options
+       */
+      async sendDeleteable(content, options) {
+        const replies = await this.reply(content, options)
+        const reply = Array.isArray(replies)
+          ? replies[replies.length - 1]
+          : replies
+        const wastebasket = '🗑️'
 
-    if (Array.isArray(replies)) {
-      const lastReply = replies[replies.length - 1]
+        const reactionFilter = (reaction, user) =>
+          reaction.emoji.name === wastebasket && user.id === this.author.id
+        const messageFilter = receiveMessage => {
+          const num = parseInt(receiveMessage.content.trim())
 
-      lastReply
-        .react(wastebasket)
-        .then(reaction =>
-          reaction.message.awaitReactions(filter, {
-            idle: 60000,
-            max: 1,
-            errors: ['idle'],
-          })
-        )
-        .then(() =>
-          Promise.all(replies.map(reply => reply.delete()).push(this.delete()))
-        )
-        .catch(() => lastReply.reactions.removeAll())
-    } else {
-      /** @type {import('discord.js').Message} */
-      const reply = replies
+          if (Number.isNaN(num)) return false
+          if (num >= 0 && num <= 2) return true
+          else return false
+        }
 
-      reply
-        .react(wastebasket)
-        .then(reaction =>
-          reaction.message.awaitReactions(filter, {
-            idle: 60000,
-            max: 1,
-            errors: ['idle'],
-          })
-        )
-        .then(() => Promise.all([message.delete(), reply.delete()]))
-        .catch(() => reply.reactions.removeAll())
+        const awaitReaction = () =>
+          reply
+            .awaitReactions(reactionFilter, {
+              max: 1,
+              idle: 60000,
+              errors: ['idle'],
+            })
+            .then(collection => collection.first())
+
+        const awaitOptionInput = () =>
+          this.channel
+            .awaitMessages(messageFilter, {
+              max: 1,
+              idle: 60000,
+              errors: ['idle'],
+            })
+            .then(collection => collection.first())
+
+        await reply.react(wastebasket)
+
+        const run = async () => {
+          const reaction = await awaitReaction().catch(() => null)
+
+          if (!reaction) return reply.reactions.removeAll()
+
+          const question = await this.channel.send(
+            new MessageEmbed()
+              .setColor('YELLOW')
+              .setTitle('削除方法を選択してください（数字）')
+              .setDescription(
+                [
+                  '0: キャンセル',
+                  '1: リザルトだけを削除する',
+                  '2: あなたが送信したコードとリザルトを削除する',
+                ].join('\n')
+              )
+          )
+          const input = await awaitOptionInput().catch(() => 0)
+
+          const option = parseInt(input.content.trim())
+
+          if (option === 1)
+            return Promise.all([
+              Array.isArray(replies)
+                ? replies.map(message => message.delete())
+                : reply.delete(),
+              question.delete(),
+              input.delete(),
+            ])
+          if (option === 2)
+            return Promise.all([
+              Array.isArray(replies)
+                ? replies.map(message => message.delete())
+                : reply.delete(),
+              question.delete(),
+              input.delete(),
+              this.delete(),
+            ])
+
+          await Promise.all([
+            reaction.users.remove(this.author),
+            question.delete(),
+            input.delete(),
+          ])
+
+          return run()
+        }
+
+        run().catch(console.error)
+      }
     }
-  }
-})
+)
