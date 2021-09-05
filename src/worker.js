259b0bd7fce2b9bfa1c49b4ed327561ae6211a0a
InkoHX <me@inkohx.dev>
2021-09-05 18:54:46 +0900

    format code

--- src/worker.js
+++ src/worker.js
@@ -2,6 +2,7 @@ const worker = require('workerpool').worker
 const { VM } = require('vm2')
 const { inspect } = require('util')
 
-const run = code => inspect(new VM().run(code), { depth: null, maxArrayLength: null })
+const run = code =>
+  inspect(new VM().run(code), { depth: null, maxArrayLength: null })
 
 worker({ run })
