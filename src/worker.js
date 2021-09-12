2dbe349bcb537d4b95f5a4608df3fc5fadbc81b4
kage-sakura <79582228+kage-sakura@users.noreply.github.com>
2021-09-12 15:55:48 +0900

    Make it possible to display data structures of collections correctly
    vm2.VMから返却されたSet, Map, Date, RegExp, WeakSet, WeakMap, ArrayBuffer, TypedArray, BoxedPrimitiveなどをutil.inspectすると、情報が抜け落ちる問題の修正

--- src/worker.js
+++ src/worker.js
@@ -1,8 +1,56 @@
-const worker = require('workerpool').worker
+const { worker } = require('workerpool')
 const { VM } = require('vm2')
 const { inspect } = require('util')
 
-const run = code =>
-  inspect(new VM().run(code), { depth: null, maxArrayLength: null })
+const run = async code => {
+  const vm = new VM({
+    sandbox: {
+      Set, Map, Date, WeakSet, WeakMap,
+      Buffer, ArrayBuffer, SharedArrayBuffer,
+      Int8Array, Uint8Array, Uint8ClampedArray,
+      Int16Array, Uint16Array,
+      Int32Array, Uint32Array,
+      Float32Array, Float64Array,
+      BigInt64Array, BigUint64Array
+    }
+  })
+  const result = await vm.run(code)
+  const VMRegExp = vm.run("RegExp")
+  VMRegExpProtoToString = VMRegExp.prototype.toString
+  Object.defineProperty(VMRegExp.prototype, inspect.custom, { 
+    value(_, options) {
+      try {
+        return VMRegExpProtoToString.call(this)
+      } catch {
+        return inspect(
+          Object.defineProperty(
+            this, inspect.custom,
+            { value: void 0 }
+          ), 
+          options
+        )
+      }
+    }
+  })
+  for (const type of ["Number", "String", "Boolean", "Symbol", "BigInt"]) {
+    const ctor = vm.run(type), toPrimitive = ctor.prototype.valueOf
+    Object.defineProperty(ctor.prototype, inspect.custom, {
+      value(_, options) {
+        try {
+          return `[${type}: ${inspect(toPrimitive.call(this))}]`
+        } catch {
+          return inspect(
+            Object.defineProperty(
+              this, inspect.custom,
+              { value: void 0 }
+            ), 
+            options
+          )
+        }
+      }
+    })
+  }
+  return inspect(result, { depth: null, maxArrayLength: null })
+}
 
 worker({ run })
