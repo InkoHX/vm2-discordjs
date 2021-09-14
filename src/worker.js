bc1e2372044f70e4e1872f9b7ff48a1e67c8d033
kage-sakura <79582228+kage-sakura@users.noreply.github.com>
2021-09-14 22:25:12 +0900

    コレクションのデータ構造明らかにする (#32)

--- src/worker.js
+++ src/worker.js
@@ -1,8 +1,74 @@
-const worker = require('workerpool').worker
+const { worker } = require('workerpool')
 const { VM } = require('vm2')
 const { inspect } = require('util')
 
-const run = code =>
-  inspect(new VM().run(code), { depth: null, maxArrayLength: null })
+const run = async code => {
+  const vm = new VM({
+    sandbox: {
+      Set,
+      Map,
+      Date,
+      WeakSet,
+      WeakMap,
+      Buffer,
+      ArrayBuffer,
+      SharedArrayBuffer,
+      Int8Array,
+      Uint8Array,
+      Uint8ClampedArray,
+      Int16Array,
+      Uint16Array,
+      Int32Array,
+      Uint32Array,
+      Float32Array,
+      Float64Array,
+      BigInt64Array,
+      BigUint64Array,
+    },
+  })
+  const vmRegExpPrototype = vm.run('RegExp').prototype,
+    vmRegExpProtoToString = vmRegExpPrototype.toString
+  const primitiveTypes = [
+    'Number',
+    'String',
+    'Boolean',
+    'Symbol',
+    'BigInt',
+  ].map(type => {
+    const { prototype } = vm.run(type)
+    return [type, prototype, prototype.valueOf]
+  })
+
+  const result = vm.run(code)
+
+  Object.defineProperty(vmRegExpPrototype, inspect.custom, {
+    value(_, options) {
+      try {
+        return vmRegExpProtoToString.call(this)
+      } catch {
+        return inspect(
+          Object.defineProperty(this, inspect.custom, { value: void 0 }),
+          options
+        )
+      }
+    },
+  })
+  for (const [type, prototype, toPrimitive] of primitiveTypes) {
+    Object.defineProperty(prototype, inspect.custom, {
+      value(_, options) {
+        try {
+          return `[${type}: ${inspect(toPrimitive.call(this))}]`
+        } catch {
+          return inspect(
+            Object.defineProperty(this, inspect.custom, { value: void 0 }),
+            options
+          )
+        }
+      },
+    })
+  }
+
+  return inspect(result, { depth: null, maxArrayLength: null })
+}
 
 worker({ run })
