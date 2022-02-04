d5da7e175291ad904f7ddc554553c062985b792a
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-04 10:44:55 +0900

    Upgrade discord.js v12 to v13 (#49)
    close #33
    
    Co-authored-by: InkoHX <inkohx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 require('./structures/message')
 
-const { Client, MessageAttachment, APIMessage, Intents } = require('discord.js')
+const {
+  Client,
+  MessageAttachment,
+  MessagePayload,
+  Intents,
+} = require('discord.js')
 const path = require('path')
 const pool = require('workerpool').pool(path.join(__dirname, './worker.js'), {
   workerType: 'process',
 })
+const { codeBlock } = require('@discordjs/builders')
+const intents =
+  Intents.FLAGS.GUILDS |
+  Intents.FLAGS.GUILD_MESSAGES |
+  Intents.FLAGS.GUILD_MESSAGE_REACTIONS
 
 const client = new Client({
-  ws: {
-    intents: Intents.NON_PRIVILEGED,
-  },
+  intents,
   presence: {
     activity: {
       name: 'JavaScript',
@@ -18,39 +26,38 @@ const client = new Client({
   },
 })
 
-const codeBlockRegex = /^`{3}(?<lang>[a-z]+)\n(?<code>[\s\S]+)\n`{3}$/mu
+const codeBlockRegex = /^`{3}(?<language>[a-z]+)\n(?<code>[\s\S]+)\n`{3}$/mu
 const languages = ['js', 'javascript']
-
 const toMessageOptions = content => {
-  if (content.length <= 2000)
-    return APIMessage.transformOptions(content, { code: 'js' })
-  else
-    return APIMessage.transformOptions(
-      '実行結果が長すぎるのでテキストファイルに出力しました。',
-      new MessageAttachment(Buffer.from(content), 'result.txt')
-    )
+  if (content.length <= 2000) return codeBlock('js', content)
+  else {
+    const file = new MessageAttachment(Buffer.from(content), 'result.txt')
+    return MessagePayload.create(message.channel, {
+      content: '実行結果が長すぎるのでテキストファイルに出力しました。',
+      files: [file],
+    })
+  }
 }
 
 client.once('ready', () => console.log('Ready'))
 
-client.on('message', message => {
+client.on('messageCreate', message => {
   if (message.author.bot || message.system) return
   if (!message.content.toLowerCase().startsWith('>runjs')) return
   if (!codeBlockRegex.test(message.content))
     return message.reply('コードを送信してください。').catch(console.error)
 
-  const codeBlock = message.content.match(codeBlockRegex)?.groups ?? {}
-
-  if (!languages.includes(codeBlock.lang))
+  const { language, code } = message.content.match(codeBlockRegex)?.groups ?? {}
+  if (!languages.includes(language))
     return message
       .reply(`言語識別子が**${languages.join(', ')}**である必要があります。`)
       .catch(console.error)
 
   pool
-    .exec('run', [codeBlock.code])
+    .exec('run', [code])
     .timeout(5000)
-    .then(result => message.sendDeleteable(toMessageOptions(result)))
-    .catch(error => message.sendDeleteable(error, { code: 'js' }))
+    .then(result => message.sendDeletable(toMessageOptions(result)))
+    .catch(error => message.sendDeletable(codeBlock('js', error)))
 })
 
 client.login().catch(console.error)
