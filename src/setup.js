be3ac479bb7492e32ca22321869b77cca6b6c0aa
kage-sakura <furandolltyan@gmail.com>
2022-06-19 03:58:01 +0000

    Store common things in variables

--- src/setup.js
+++ src/setup.js
@@ -1,35 +1,45 @@
-Object.defineProperty(
-  RegExp.prototype,
-  Symbol.for('nodejs.util.inspect.custom'),
+'use strict'
+
+{
+  const { call } = Function.prototype
+  const { defineProperty } = Object
+  const inspectCustom = Symbol.for('nodejs.util.inspect.custom')
+
   {
-    value: RegExp.prototype.toString,
+    const { prototype } = RegExp
+    defineProperty(prototype, inspectCustom, {
+      value: prototype.toString,
+    })
   }
-)
 
-{
-  const { isNaN } = Number,
-    { prototype } = Date,
-    { call } = Function.prototype,
-    getTime = call.bind(prototype.getTime),
-    toString = call.bind(prototype.toString),
-    toISOString = call.bind(prototype.toISOString)
-  Object.defineProperty(prototype, Symbol.for('nodejs.util.inspect.custom'), {
-    value() {
-      return isNaN(getTime(this)) ? toString(this) : toISOString(this)
-    },
-  })
-}
+  {
+    const { isNaN } = Number
+    const { prototype } = Date
+    const getTime = call.bind(prototype.getTime)
+    const toString = call.bind(prototype.toString)
+    const toISOString = call.bind(prototype.toISOString)
+    defineProperty(prototype, inspectCustom, {
+      value() {
+        try {
+          return isNaN(getTime(this)) ? toString(this) : toISOString(this)
+        } catch {
+          return this
+        }
+      },
+    })
+  }
 
-{
-  const call = Function.prototype.call.bind(Function.prototype.call),
-    proxify = (value, construct, getters) =>
+  {
+    const funcCall = call.bind(call)
+    const proxify = (value, construct, getters) =>
       new Proxy(value, {
         __proto__: null,
+        construct,
         get(_, key, receiver) {
           const getter = getters(key)
-          return getter ? call(getter, receiver) : value[key]
+          return getter ? funcCall(getter, receiver) : value[key]
         },
-        construct,
       })
-  proxify
+    proxify
+  }
 }
